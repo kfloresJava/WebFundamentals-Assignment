c3181b5705d5093ac363b1304724d2eeff0e22b3
kfloresJava <88413754+kfloresJava@users.noreply.github.com>
2021-09-16 09:50:09 +0800

    Add files via upload

---
+++ Likes/myjs.js
@@ -0,0 +1,21 @@
+function increm1()
+{
+ let sb= document.querySelector("#numx1");
+ let nx=parseInt(sb.textContent)+1;
+ alert("An idea was liked");
+ sb.textContent=nx;
+}
+function increm2()
+{
+ let sb= document.querySelector("#numx2");
+ let nx=parseInt(sb.textContent)+1;
+ alert("An idea was liked");
+ sb.textContent=nx;
+}
+function increm3()
+{
+ let sb= document.querySelector("#numx3");
+ let nx=parseInt(sb.textContent)+1;
+ alert("An idea was liked");
+ sb.textContent=nx;
+}
